a2708c642b38d3d0308075ba5bd24d2119f90c37
dusty <dwevans2203@gmail.com>
2025-08-20 22:04:43 +0100

    rotation matrix added

--- RobotArm_ForwardKinematics/main.cpp
+++ RobotArm_ForwardKinematics/main.cpp
@@ -1,24 +1,38 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 #include <vector>
 
 struct Joints
 {
-  double length;
-  double angle;
+    double length{};
+    double angle{};
 };
 
-std::vector<std::vector<double>> rotationMatrix (double angle)
+struct coords
 {
-  return {{std::cos(angle), -std::sin(angle)}};
+    double x{};
+    double y{};
+};
+
+std::vector<std::vector<double>> rotationMatrix(double angle)
+{
+    return {{std::cos(angle), -std::sin(angle)}, {std::sin(angle), std::cos(angle)}};
+}
+
+coords applyRotation(coords c, std::vector<std::vector<double>> r)
+{
+    coords result{};
+    result.x = r[0][0] * c.x + r[1][0] * c.x;
+    result.y = r[1][1] * c.y + r[0][1] * c.y;
+    return result;
 }
 int main()
 {
-  std::vector<std::vector<double>> matrix;
-  Joints l1{10, 1};
-  std::vector<std::vector<double>> rotation = rotationMatrix(l1.angle);
-  for(int i{};i< rotation.size(); ++i)
-    for(int j{};j< rotation.size(); ++j)
-      std::cout << rotation[i][j] << std::endl;
-  return 0;
+    coords arm{2.0, 2.0};
+    Joints l1{10.0, 1.0};
+
+    coords r{applyRotation(arm, rotationMatrix(360.0))};
+    std::cout << r.x << ',' << r.y << std::endl;
+
+    return 0;
 }
