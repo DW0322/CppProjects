074fc33fe5af64ad4d3c723630915df051d89415
dusty <dwevans2203@gmail.com>
2025-08-25 20:06:26 +0100

    Improved coordinates using angle of arm and the angle. Simplified it by using trigonometric ratios.

--- RobotArm_ForwardKinematics/main.cpp
+++ RobotArm_ForwardKinematics/main.cpp
@@ -1,37 +1,35 @@
-#include <iostream>
-#include <vector>
 #include <cmath>
+#include <iostream>
 
 struct Coords
 {
-  double x;
-  double y;
+    double x;
+    double y;
 };
 
 struct Joints
 {
-  double length;
-  double angle;
-  Coords coords;
+    double length;
+    double angle;
 };
 
 double degreeToRads(double angle)
 {
-  return angle * (M_PI/180);
+    return angle * (M_PI / 180);
 }
 
-Coords rotate(const Coords &arm, double angle)
+Coords armCoords(Joints arm)
 {
-  Coords NewCoords{};
-  NewCoords.x = arm.x * std::cos(angle) - std::sin(angle) * arm.y;
-  NewCoords.y = arm.x * std::sin(angle) + std::cos(angle) * arm.y;
-  return NewCoords;
+    Coords c;
+    c.x = std::sin(arm.angle) * arm.length;
+    c.y = std::cos(arm.angle) * arm.length;
+    return c;
 }
 
 int main()
 {
-  Joints l1 {10, 80, {1, 10.0}};
-  Coords NewArm {rotate(l1.coords, degreeToRads(l1.angle))};
-  std::cout << NewArm.x << ',' << NewArm.y << std::endl;
-  return 0;
+    Joints l1{10.0, degreeToRads(45.0)};
+    Coords c{armCoords(l1)};
+    std::cout << c.x << ',' << c.y << std::endl;
+    return 0;
 }
